a569e0eb60a763eafec3bdc4c0d105289fb67dbf
Melissa A <41melissa.allen@gmail.com>
2018-08-23 06:31:21 -0400

    updating data

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+###SEEDS
+INSERT INTO burgers (burger_name,devoured) VALUES ('Best burger of all time', true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Worst burger of all time', false);
+
---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
