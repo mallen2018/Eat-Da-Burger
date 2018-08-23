7fa33c3afd4b3da0236ebd4e85d7bbc2735276af
Melissa A <41melissa.allen@gmail.com>
2018-08-23 06:37:00 -0400

    update data

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ var mysql = require("mysql");
 
 var connection;
 
-if(process.env.burger_URL) {
-    connection = mysql.createConnection(process.env.burger_URL);
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
     connection = mysql.createConnection({
