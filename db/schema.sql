339f37306b2be13cc40e6417ea8756d89828bc51
Melissa A <41melissa.allen@gmail.com>
2018-08-23 21:58:04 -0400

    update

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE database burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
- `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
+ id Int( 11 ) AUTO_INCREMENT NOT NULL,
   burger_name VARCHAR(100) NULL,
   devoured boolean,
   PRIMARY KEY (id)
